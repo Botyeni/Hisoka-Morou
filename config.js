5dd9a437e3da8f750dcba3e2046ea1b9aca2c9f2
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2022-04-07 10:02:07 +0800

    CR : Mas Dika
    ganteng tehe

--- config.js
+++ config.js
@@ -14,33 +14,33 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapi.xyz': 'bagi dong:(',
+	'https://zenzapi.xyz': 'cb2f7f0302', //hai kang recoder, bikin api sendiri di zenzapi.xyz pake email, jangan pake punya saya.
 }
 
 // Other
-global.owner = ['6281998903280','1']
+global.owner = ['6281998903280','16124515978']
 global.premium = ['6288292024190']
 global.packname = 'RaidenBot'
-global.author = 'oleh kyura\n\n\ntemukan raiden di\ntelegram bit.ly/telekanao'
-global.sessionName = 'kyura'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.author = 'oleh kyura\n\n\ntemukan raidenbot di\ntelegram bit.ly/telekanao'
+global.sessionName = 'sessionmultidevice'
+global.prefa = ['','!','.','k','#']
 global.sp = '⭔'
 global.mess = {
-    success: '✓ Success',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...',
+    success: '*Berhasil !*\n\n\n_Bantu Subscribe youtube.com/c/KYURA_',
+    admin: 'Fitur Khusus Admin Group.',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu.',
+    owner: 'Fitur Khusus Owner Bot.',
+    group: 'Fitur Digunakan Hanya Untuk Group.',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat.',
+    bot: 'Fitur Khusus Pengguna Nomor Bot.',
+    wait: '*Working on it...*\n\n\n\n_Jika tidak respon, berarti ada kesalahan, hubungi owner/coba lagi nanti._',
     endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
 global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.thumb = fs.readFileSync('./lib/kyura.jpg')
+global.thumb = fs.readFileSync('./lib/thumbraiden.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
